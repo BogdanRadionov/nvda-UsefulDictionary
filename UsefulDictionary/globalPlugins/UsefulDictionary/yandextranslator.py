791b83d668b160dbd4f0cac2df3cc4f8b2d9cd50
Bogdan Radionov <radionov.bogdan@gmail.com>
2016-04-03 20:28:15 +0300

    Теперь Yandex translator умеет принимать многострочный текст; Обновлен установочный пакет.

--- UsefulDictionary/globalPlugins/UsefulDictionary/yandextranslator.py
+++ UsefulDictionary/globalPlugins/UsefulDictionary/yandextranslator.py
@@ -12,7 +12,7 @@ name = _(u'Yandex translator')
 url_request = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160311T160508Z.34101ab79242d6fd.120d4f5b06e2efd6dfbd194e5aa9756df01bfcda&text={text}&lang={lang}'
 
 def sendRequest(text, lang):
-	text = text.encode('utf-8')
+	text = urllib2.quote(text.encode('utf-8'))
 	return json.load(urllib2.urlopen(url_request.format(text=text, lang=lang)))
 
 def parseResponse(response):
